8323dc9999d96c4aa4265c84bb7e4df53bd3c3c8
Jeremy Kun <j2kun@users.noreply.github.com>
2022-10-25 17:34:59 +0000

    add function for fidning build target

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -24,4 +24,14 @@ vim.keymap.set('n', '<leader>u', 'viwu')
 vim.keymap.set('n', '<leader>ev', ':vsplit <C-R>=expand("%:p:h") . "/" <CR>')
 vim.keymap.set('n', '<leader>es', ':split <C-R>=expand("%:p:h") . "/" <CR>')
 vim.keymap.set('n', '<leader>ee', ':edit <C-R>=expand("%:p:h") . "/" <CR>')
-vim.keymap.set('n', '<leader>eb', ':vsplit <C-R>=expand("%:p:h") . "/BUILD" <CR>')
+
+vim.keymap.set('n', '<leader>eb', function()
+  -- expand("%:p:h") gets the current filepath
+  local buildfile = vim.fn.expand("%:p:h") .. "/BUILD"
+  -- expand("%:t") gets the current filename with suffix.
+  local target = vim.fn.expand("%:t")
+  vim.api.nvim_command("botright vsplit " .. buildfile)
+  vim.cmd("normal /" .. target .. vim.api.nvim_replace_termcodes("<CR>", true, true, true))
+  vim.cmd("normal zz")
+end,
+  {noremap = true})
