5d71c5c9355ed9cdc964ba2f1146ba1ba676d28e
Jeremy Kun <j2kun@users.noreply.github.com>
2023-03-15 11:38:58 -0700

    remove line causing error in cmp

--- lua/cmp_config.lua
+++ lua/cmp_config.lua
@@ -21,7 +21,7 @@ cmp.setup({
       local kind = lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
       local strings = vim.split(kind.kind, "%s", { trimempty = true })
       kind.kind = " " .. strings[1] .. " "
-      kind.menu = "    (" .. strings[2] .. ")"
+      -- kind.menu = "    (" .. strings[2] .. ")"
 
       return kind
     end,
