e71f77b9863d57abee1df59569e49450b445666a
Jeremy Kun <j2kun@users.noreply.github.com>
2023-04-11 12:18:11 -0700

    add python width ftplugin

---
+++ ftplugin/python.lua
@@ -0,0 +1,4 @@
+vim.opt.shiftwidth = 2   -- number of visual spaces per shift
+vim.opt.softtabstop = 2  -- number of spaces in tab when editing
+vim.opt.tabstop = 2      -- number of visual spaces per TAB
+
