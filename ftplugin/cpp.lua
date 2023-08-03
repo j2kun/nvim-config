54e043fe531684c6b388af48c92f0f3e006b6d18
Jeremy Kun <j2kun@users.noreply.github.com>
2023-08-03 10:53:42 -0700

    use telescope for header/cc navigation

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -1,19 +1,27 @@
+local try_open_in_telescope = function(filename)
+  local status, ts_builtin = pcall(require, "telescope.builtin")
+  if(status) then
+    ts_builtin.find_files({search_file = filename})
+  else
+    -- fall back if telescope is not installed: just search in current cwd
+    -- expand("%:p:h") gets the current filepath
+    local path = vim.fn.expand("%:p:h")
+    vim.api.nvim_command("botright vsplit " .. path .. "/" .. filename)
+  end
+end
+
 -- Go to header file for the current cc file
 vim.keymap.set('n', '<leader>eh', function()
-  -- expand("%:p:h") gets the current filepath
-  local path = vim.fn.expand("%:p:h")
   -- expand("%:t:r") gets the current filename without suffix
   local filename = vim.fn.expand("%:t:r") .. ".h"
-  vim.api.nvim_command("botright vsplit " .. path .. "/" .. filename)
+  try_open_in_telescope(filename)
 end,
   {noremap = true})
 
 -- Go to cc file for the current header file
 vim.keymap.set('n', '<leader>ec', function()
-  -- expand("%:p:h") gets the current filepath
-  local path = vim.fn.expand("%:p:h")
   -- expand("%:t:r") gets the current filename without suffix
   local filename = vim.fn.expand("%:t:r") .. ".cc"
-  vim.api.nvim_command("botright vsplit " .. path .. "/" .. filename)
+  try_open_in_telescope(filename)
 end,
   {noremap = true})
