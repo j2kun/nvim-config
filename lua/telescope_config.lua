6c5858d906571193b119cbd7bca82144f58d23f8
Jeremy Kun <j2kun@users.noreply.github.com>
2024-07-04 21:40:57 -0700

    improve telescope config

--- lua/telescope_config.lua
+++ lua/telescope_config.lua
@@ -85,13 +85,11 @@ vim.keymap.set('n', '<leader>fl', ':Telescope builtin<cr>')
 
 -- For more common options, a dedicated keymap
 vim.keymap.set('n', '<c-p>', ':Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files<cr>')
 vim.keymap.set('n', '<leader>fs', ':Telescope grep_string<cr>')
 vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>fb', ':Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<cr>')
 vim.keymap.set('n', '<leader>fr', ':Telescope registers<cr>')
-vim.keymap.set('n', '<leader>fj', ':Telescope jump_list<cr>')
 vim.keymap.set('n', '<leader>fo', ':Telescope oldfiles<cr>')
 vim.keymap.set('n', '<leader>fm', ':Telescope marks<cr>')
 vim.keymap.set('n', '<leader>fp', ':Telescope projects<cr>')
