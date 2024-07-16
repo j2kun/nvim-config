3daede65d29ec980198866fda98a67fd18adcec3
Jeremy Kun <j2kun@users.noreply.github.com>
2024-07-16 07:39:47 -0700

    add sticky yank

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -8,3 +8,23 @@ api.nvim_create_autocmd("BufWritePre", {
   command = ":%s/\\s\\+$//e",
   group = trimGroup,
 })
+
+-- "sticky yank" keep the cursor at the same position after yanking
+-- https://nanotipsforvim.prose.sh/sticky-yank
+local cursorPreYank
+vim.keymap.set({ "n", "x" }, "y", function()
+	cursorPreYank = vim.api.nvim_win_get_cursor(0)
+	return "y"
+end, { expr = true })
+vim.keymap.set("n", "Y", function()
+	cursorPreYank = vim.api.nvim_win_get_cursor(0)
+	return "y$"
+end, { expr = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		if vim.v.event.operator == "y" and cursorPreYank then
+			vim.api.nvim_win_set_cursor(0, cursorPreYank)
+		end
+	end,
+})
